8100553c2929f4d8b5ed2421f342e5cc186ea572
hugofrid <hugofrid2@gmail.com>
2020-04-21 16:41:18 +0200

    config for gh-page deploy

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
-
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>< App /></Router>,
+  < App />,
   document.getElementById('root')
 );
 
